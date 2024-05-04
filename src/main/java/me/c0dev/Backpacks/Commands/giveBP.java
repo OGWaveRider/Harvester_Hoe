213f001d6b23065efebae6fa734736bb939d1a19
OGWaveRider <camerontowver@gmail.com>
2024-05-03 22:05:38 -0700

    Forgot command :(

--- src/main/java/me/c0dev/Backpacks/Commands/giveBP.java
+++ src/main/java/me/c0dev/Backpacks/Commands/giveBP.java
@@ -1,10 +1,14 @@
 package me.c0dev.Backpacks.Commands;
 
 import me.c0dev.Main;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import me.c0dev.Backpacks.Items.Backpack;
 
 public class giveBP implements CommandExecutor {
     public Main main;
@@ -17,7 +21,13 @@ public class giveBP implements CommandExecutor {
         Player player = (Player) sender;
         String type = args[0];
         if (player.isOp()) {
-
+            if (player.getInventory().firstEmpty() == -1) {
+                player.sendMessage(ChatColor.DARK_RED + "[!] " + ChatColor.RED + "You're Inventory is full!");
+                return false;
+            }
+            ItemStack backpack = Backpack.createBackpack(type);
+            player.getInventory().addItem(backpack);
+            return true;
         }
         return false;
     }
